da1fca09b7795f5d5369cc80fb4604cda7e991f3
Elias Youssef <d-s-a00@hotmail.com>
2019-03-13 22:46:43 +0100

    url en html helpers weg gehaald omdat ze nou automatisch worden geladen

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-        $this->load->helper('url');
-        $this->load->helper('html');
         $data['title'] = 'To do list';
         $this->load->view('templates/header_view', $data);
 		$this->load->view('Home_view');
