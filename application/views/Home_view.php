0c2c3e5ef993844e9d900827fd2dbae25029e99d
Elias Youssef <d-s-a00@hotmail.com>
2019-03-13 22:46:58 +0100

    betere indeling

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -1,30 +1,84 @@
-<div class="list-group col-md-3 col-sm-6">
-    <div class="list-group-item list-group-item-action active">
-        Cras justo odio
+<div id="toDoListsMainCon" class="d-inline-flex">
+    <div class="list-group toDoLists">
+        <div class="list-group-item list-group-item-action list-group-item-primary active">
+            Cras justo odio
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck1">
+            <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck2">
+            <label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck3">
+            <label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck4">
+            <label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck5">
+            <label class="custom-control-label" for="customCheck5">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
     </div>
-    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
-        <input type="checkbox" class="custom-control-input" id="customCheck1">
-        <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
-        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+    <div class="list-group toDoLists">
+        <div class="list-group-item list-group-item-action list-group-item-primary active">
+            Cras justo odio
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck1">
+            <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck2">
+            <label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck3">
+            <label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck4">
+            <label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
+        <div class="custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5">
+            <input type="checkbox" class="custom-control-input" id="customCheck5">
+            <label class="custom-control-label" for="customCheck5">Check this custom checkbox</label>
+            <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+        </div>
     </div>
-    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
-        <input type="checkbox" class="custom-control-input" id="customCheck2">
-        <label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
-        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
-    </div>
-    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
-        <input type="checkbox" class="custom-control-input" id="customCheck3">
-        <label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
-        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
-    </div>
-    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
-        <input type="checkbox" class="custom-control-input" id="customCheck4">
-        <label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
-        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
-    </div>
-    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
-        <input type="checkbox" class="custom-control-input" id="customCheck5">
-        <label class="custom-control-label" for="customCheck5">Check this custom checkbox</label>
-        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+</div>
+<button type="button" class="btn btn-newList inline-block col-1" id="newListBtn"><i class="fas fa-plus"></i> New list</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
 </div>
