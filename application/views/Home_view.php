f8d559b4f60d2bea319a4bdb95f3646a492f1633
Elias Youssef <d-s-a00@hotmail.com>
2019-03-13 15:10:17 +0100

    start

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -1,89 +1,30 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+<div class="list-group col-md-3 col-sm-6">
+    <div class="list-group-item list-group-item-action active">
+        Cras justo odio
+    </div>
+    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
+        <input type="checkbox" class="custom-control-input" id="customCheck1">
+        <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+    </div>
+    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
+        <input type="checkbox" class="custom-control-input" id="customCheck2">
+        <label class="custom-control-label" for="customCheck2">Check this custom checkbox</label>
+        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+    </div>
+    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
+        <input type="checkbox" class="custom-control-input" id="customCheck3">
+        <label class="custom-control-label" for="customCheck3">Check this custom checkbox</label>
+        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+    </div>
+    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
+        <input type="checkbox" class="custom-control-input" id="customCheck4">
+        <label class="custom-control-label" for="customCheck4">Check this custom checkbox</label>
+        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+    </div>
+    <div class="custom-control custom-checkbox list-group-item list-group-item-action pl-5">
+        <input type="checkbox" class="custom-control-input" id="customCheck5">
+        <label class="custom-control-label" for="customCheck5">Check this custom checkbox</label>
+        <i class="fas fa-edit text-primary float-right listItemEditBtns"></i>
+    </div>
 </div>
-
-</body>
-</html>
