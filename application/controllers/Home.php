165765e1536df2a346199ce3664a078e67805716
Elias Youssef <d-s-a00@hotmail.com>
2019-03-14 21:47:45 +0100

    verbinding naar de model

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,8 @@ class Home extends CI_Controller {
 	public function index()
 	{
         $data['title'] = 'To do list';
+        $this->load->model( 'Home_model' );
+        var_dump($this->Home_model->getAllLists());
         $this->load->view('templates/header_view', $data);
 		$this->load->view('Home_view');
         $this->load->view('templates/footer_view');
