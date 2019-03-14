182b0fb867c5061335cb5bc554876b22f9b28ad2
Elias Youssef <d-s-a00@hotmail.com>
2019-03-15 00:33:51 +0100

    de allLists method haalt de lijsten uit de database en convert ze naar json

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,10 +5,15 @@ class Home extends CI_Controller {
 	public function index()
 	{
         $data['title'] = 'To do list';
-        $this->load->model( 'Home_model' );
-        var_dump($this->Home_model->getAllLists());
         $this->load->view('templates/header_view', $data);
 		$this->load->view('Home_view');
         $this->load->view('templates/footer_view');
+	}
+
+	public function allLists()
+	{
+        $this->load->model( 'Home_model' );
+        $lists = $this->Home_model->getAllLists();
+        echo(json_encode($lists));
 	}
 }
