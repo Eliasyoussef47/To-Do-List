ae7418f34816b6883a789ca39834ada25adb8e67
Elias Youssef <d-s-a00@hotmail.com>
2019-03-16 21:28:31 +0100

    dit is niet nodig

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,3 @@
 window.onload = function() {
     getListsFromServer().then(response => makeLists(response));
 };
-editModal.onclick = function() {
-    if (event.target.dataset["function"] !== undefined && event.target.dataset["function"] === "delete") {
-        deleteBtn(this);
-    }
-};
