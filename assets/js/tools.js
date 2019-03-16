6374f5129435a8f5e4e9f945e66f2bb3cbad0a9d
Elias Youssef <d-s-a00@hotmail.com>
2019-03-16 03:24:42 +0100

    makeLists aangepast zodat het de nieuwe html structuur kan uitvoeren

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -38,61 +38,85 @@ function stopMediumLoading() {
     hideElementT(fullScreanLoadingBackground);
 }
 
-function getListsFromServer() {
-    startFullScreenLoading();
-    const myInit = {
-        method: 'GET',
-        credentials: 'include'
-    };
-
-    fetch("http://localhost/To-Do-List/home/alllists", myInit)
-    .then(function(response) {
-        if (response.status === 200) {
-            return response.json();
-        }
-    }).then(function (json) {
-        makeLists(json);
-        stopFullScreenLoading();
-    }).catch(function (error) {
-        return error;
-    });
-}
-
 function makeLists(array) {
     let currentListId = "0";
     let listConDiv;
     let listHeader;
+    let listHeaderTitle;
+    let listBody;
     let listItem;
     let listItemCheckbox;
     let listItemCheckboxLabel;
     let listItemEditBtn;
-
+    let listItemOptionsBtnWrap;
+    let listItemOptionsBtn;
+    let listItemOptionsBtnDropdownMenu;
+    let listItemOptionsBtnDropdownMenuOption;
+    let listFooter;
+    let listItemAddBtns;
+    let nextArrayElement;
     array.forEach(function(currentElm, index, array) {
+        nextArrayElement = array[(index + 1)];
         if (currentListId !== currentElm.listId) {
             listConDiv = document.createElement("DIV");
             listConDiv.className = "list-group toDoLists";
             listHeader = document.createElement("DIV");
-            listHeader.className = "list-group-item list-group-item-action list-group-item-primary active";
-            listHeader.innerText = currentElm.listName;
+            listHeader.className = "list-group-item list-group-item-action list-group-item-primary active toDoListHeader";
+            listHeaderTitle = document.createElement("SPAN");
+            listHeaderTitle.innerText = currentElm.listName;
+            listHeader.appendChild(listHeaderTitle);
+            listItemEditBtn = document.createElement("I");
+            listItemEditBtn.className = "fas fa-edit text-primary float-right listItemEditBtns";
+            listItemOptionsBtnWrap = document.createElement("DIV");
+            listItemOptionsBtnWrap.className = "btn-group dropleft float-right ml-2";
+            listItemOptionsBtn = document.createElement("I");
+            listItemOptionsBtn.className = "fas fa-ellipsis-v text-primary float-right ml-2 listItemOptionsBtns";
+            listItemOptionsBtn.dataset.toggle = "dropdown";
+            listItemOptionsBtnWrap.appendChild(listItemOptionsBtn);
+            listItemOptionsBtnDropdownMenu = document.createElement("DIV");
+            listItemOptionsBtnDropdownMenu.className = "dropdown-menu";
+            listItemOptionsBtnDropdownMenuOption = document.createElement("A");
+            listItemOptionsBtnDropdownMenuOption.className = "dropdown-item";
+            listItemOptionsBtnDropdownMenuOption.href = "#";
+            listItemOptionsBtnDropdownMenuOption.innerText = "Delete";
+            listItemOptionsBtnDropdownMenu.appendChild(listItemOptionsBtnDropdownMenuOption);
+            listItemOptionsBtnWrap.appendChild(listItemOptionsBtnDropdownMenu);
+            listHeader.appendChild(listItemOptionsBtnWrap);
+            listHeader.appendChild(listItemEditBtn);
             listConDiv.appendChild(listHeader);
             toDoListsMainCon.appendChild(listConDiv);
             currentListId = currentElm.listId;
+            listBody = document.createElement("DIV");
+            listBody.className = "list-group-item toDoListBody";
         }
-        listItem = document.createElement("DIV");
-        listItem.className = "custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5";
-        listItemCheckbox = document.createElement("INPUT");
-        listItemCheckbox.type = "checkbox";
-        listItemCheckbox.className = "custom-control-input";
-        listItemCheckbox.id = "customCheck" + currentElm.listId + "_" + currentElm.itemId;
-        listItem.appendChild(listItemCheckbox);
-        listItemCheckboxLabel = document.createElement("LABEL");
-        listItemCheckboxLabel.className = "custom-control-label";
-        listItemCheckboxLabel.setAttribute("for", "customCheck" + currentElm.listId + "_" + currentElm.itemId);
-        listItemCheckboxLabel.innerText = currentElm.itemName;
-        listItem.appendChild(listItemCheckboxLabel);
-        listItemEditBtn = document.createElement("i");
-        listItemEditBtn.className = "fas fa-edit text-primary float-right listItemEditBtns";
-        listItem.appendChild(listItemEditBtn);
-        listConDiv.appendChild(listItem);
+        if (currentElm.itemId != null) {
+            listItem = document.createElement("DIV");
+            listItem.className = "custom-control custom-checkbox list-group-item listItems list-group-item-action pl-5";
+            listItemCheckbox = document.createElement("INPUT");
+            listItemCheckbox.type = "checkbox";
+            listItemCheckbox.className = "custom-control-input";
+            listItemCheckbox.id = "listItemCheckbox" + currentElm.listId + "_" + currentElm.itemId;
+            listItem.appendChild(listItemCheckbox);
+            listItemCheckboxLabel = document.createElement("LABEL");
+            listItemCheckboxLabel.className = "custom-control-label";
+            listItemCheckboxLabel.setAttribute("for", "listItemCheckbox" + currentElm.listId + "_" + currentElm.itemId);
+            listItemCheckboxLabel.innerText = currentElm.itemName;
+            listItem.appendChild(listItemCheckboxLabel);
+            listItemEditBtn = document.createElement("I");
+            listItemEditBtn.className = "fas fa-edit text-primary float-right listItemEditBtns";
+            listItem.appendChild(listItemEditBtn);
+            listBody.appendChild(listItem);
+        }
+        if ((nextArrayElement === undefined || nextArrayElement.listId === null) || parseInt(nextArrayElement.listId) > parseInt(currentElm.listId)) {//als de volgende listId hoger is dan de huidige
+            listConDiv.appendChild(listBody);
+            listFooter = document.createElement("A");
+            listFooter.href = "#";
+            listFooter.className = "list-group-item list-group-item-action list-group-item-primary toDoListFooter";
+            listFooter.innerText = "Add a new card";
+            listItemAddBtns = document.createElement("I");
+            listItemAddBtns.className = "fas fa-plus float-right listItemAddBtns";
+            listFooter.appendChild(listItemAddBtns);
+            listConDiv.appendChild(listFooter);
+        }
     })
 }
