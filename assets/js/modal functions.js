8c7f71de39e26504718b67516eef93fd58d563eb
Elias Youssef <d-s-a00@hotmail.com>
2019-03-16 21:28:02 +0100

    setUpModal zorgt ervor dat de goede titel en elementen in de modal komen een welke functie uitgevoerd wordt onsubmit getModalForm maakt de elementen voor in de model

--- assets/js/modal functions.js
+++ assets/js/modal functions.js
@@ -1,9 +1,52 @@
-function setUpModal(modal, title, mode) {
+function setUpModal(modal, title, mode, data) {
+    let relevantServerFunction = {
+        "updateList" : updateList
+    };
+    let relevantVisualServerFunction = {
+        "updateList" : updateListVisual
+    };
+    let modalBody = modal.querySelector("div.modal-body");
+    let modalForm = modal.querySelector("form.modalForm");
     modal.querySelector(".modal-title").innerHTML = title;
-    modal.querySelector(".modal-body").innerHTML = modalBodies[mode];
+    removeChildren(modalBody);
+    modalBody.appendChild(getModalForm(mode, data));
+    modalForm.onsubmit = function () {
+        event.preventDefault();
+        relevantServerFunction[mode](getFormValues(modalForm));
+        relevantVisualServerFunction[mode](getFormValues(modalForm));
+        $("#" + modal.id).modal("hide");
+    };
 }
 
-function getEditModalForm(mode) {
-    let modes = ["list", "listItem", "newList"];
-
+function getModalForm(mode, data) {
+    let modes = ["updateList", "updateListItem", "newList"];
+    if (!modes.includes(mode)) {
+        return false;
+    }
+    let formGroupDiv;
+    let formGroupDivLabel;
+    let formGroupDivInput;
+    if (mode === "updateList") {
+        formGroupDiv = document.createElement("DIV");
+        formGroupDiv.className = "form-group";
+        formGroupDivLabel = document.createElement("LABEL");
+        formGroupDivLabel.setAttribute("for", "listNameInput");
+        formGroupDivLabel.innerText = "List name:";
+        formGroupDiv.appendChild(formGroupDivLabel);
+        formGroupDivInput = document.createElement("INPUT");
+        formGroupDivInput.type = "text";
+        formGroupDivInput.name = "listId";
+        formGroupDivInput.hidden = true;
+        formGroupDivInput.value = data.listId;
+        formGroupDiv.appendChild(formGroupDivInput);
+        formGroupDivInput = document.createElement("INPUT");
+        formGroupDivInput.type = "text";
+        formGroupDivInput.id = "listNameInput";
+        formGroupDivInput.className = "form-control";
+        formGroupDivInput.name = "listName";
+        formGroupDivInput.value = data.listName;
+        formGroupDivInput.placeholder = "List name";
+        formGroupDiv.appendChild(formGroupDivInput);
+        return formGroupDiv;
+    }
 }
