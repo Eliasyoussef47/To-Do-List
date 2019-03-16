9d9ac1761ce8a6e3a8acedd866fb7c335d90a3a6
Elias Youssef <d-s-a00@hotmail.com>
2019-03-16 03:21:36 +0100

    test dingen gezet voor modals

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -1,20 +1,33 @@
 <div id="toDoListsMainCon" class="d-inline-flex"></div>
 <button type="button" class="btn btn-newList inline-block col-1" id="newListBtn"><i class="fas fa-plus"></i> New list</button>
 
+<button type="button" class="btn btn-primary mx-2 hFit" data-toggle="modal" data-target="#editModal">//TEST
+    Launch demo modal
+</button>
+
+<script>
+    // butto.onclick = function () {
+    //     setUpModal(editModal, "HEEEY", "list");
+    // }
+</script>
+
 <img id="rightBottomLoadingCircle" src="<?php echo base_url();?>assets/images/loading-red.svg" alt="Loading...">
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <h5 class="modal-title" id="editModalTitle">Edit list</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="form-group">
+                    <label for="listNameInput">List name:</label>
+                    <input type="text" class="form-control" id="listNameInput" placeholder="List name">
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -28,7 +41,5 @@
     <img id="fullScreanLoadingCircle" src="<?php echo base_url();?>assets/images/loading.svg" alt="Loading...">
 </div>
 <script>
-    window.onload = function () {
-        getListsFromServer();
-    };
+
 </script>
