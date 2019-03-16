053dfedce6002f3c64d5f6bf5189916c27d3af05
Elias Youssef <d-s-a00@hotmail.com>
2019-03-16 21:28:54 +0100

    updateList gemaakt

--- assets/js/server functions.js
+++ assets/js/server functions.js
@@ -6,15 +6,36 @@ function getListsFromServer() {
     };
 
     return fetch("http://localhost/To-Do-List/API/getAllLists", myInit)
-    .then(function(response) {
-        if (response.status === 200) {
-            return response.json();
-        }
-    }).then(function (json) {
-        stopFullScreenLoading();
-        return json;
+        .then(function(response) {
+            if (response.status === 200) {
+                return response.json();
+            }
+        }).then(function (json) {
+            stopFullScreenLoading();
+            return json;
 
-    }).catch(function (error) {
-        return error;
-    });
+        }).catch(function (error) {
+            return error;
+        });
+}
+
+function updateList(updateData) {
+    startMediumLoading();
+    let updateDataString = JSON.stringify(updateData);
+    let searchParams = new URLSearchParams();
+    searchParams.append('updateData', updateDataString);
+    const myInit = {
+        method: 'POST',
+        credentials: 'include',
+        body: searchParams
+    };
+    return fetch("http://localhost/To-Do-List/API/updateList", myInit)
+        .then(function(response) {
+            stopMediumLoading();
+            return response.text();
+        }).then(function(responseText) {
+            console.log(responseText);
+        }).catch(function (error) {
+            console.log(error.body);
+        });
 }
