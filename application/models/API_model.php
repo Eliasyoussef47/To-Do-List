828c07abdb8773bde7c0e8c22d780cb91d1060fb
Elias Youssef <d-s-a00@hotmail.com>
2019-03-17 01:39:30 +0100

    get list en get list item toegevoegd

--- application/models/API_model.php
+++ application/models/API_model.php
@@ -13,6 +13,31 @@ class API_model extends CI_Model {
 		}
 	}
 
+    public function getList($requestData) {
+        $this->load->database();
+        $this->db->select('*');
+		$this->db->where('listId', $requestData["listId"]);
+		$query = $this->db->get('lists');
+        if ($query->num_rows() > 0) {
+            return $query->row_array();
+        } else {
+            return false;
+        }
+    }
+
+    public function getListItem($requestData) {
+        $this->load->database();
+        $this->db->select('*');
+		$this->db->where('listItemId', $requestData["listItemId"]);
+		$this->db->where('listId', $requestData["listId"]);
+		$query = $this->db->get('listsItems');
+        if ($query->num_rows() > 0) {
+            return $query->row_array();
+        } else {
+            return false;
+        }
+    }
+
 	public function updateList($updateData) {
         $this->load->database();
         $listId = $updateData["listId"];
