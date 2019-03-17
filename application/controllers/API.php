0fb067f374f5c38bdf93c4928705c987193c6fa6
Elias Youssef <d-s-a00@hotmail.com>
2019-03-17 01:39:19 +0100

    get list en get list item toegevoegd

--- application/controllers/API.php
+++ application/controllers/API.php
@@ -11,18 +11,45 @@ class API extends CI_Controller {
     public function getAllLists()
     {
         $this->load->model('API_model');
-        $lists = $this->API_model->getAllLists();
-//        var_dump($lists);
-        echo(json_encode($lists));
+        $query = $this->API_model->getAllLists();
+        echo(json_encode($query));
+    }
+
+    public function getList()
+    {
+        if ($this->input->post('requestData') === null) {
+            http_response_code(400);
+            echo '<h1>400 Bad Request</h1>';
+        } else {
+            $requestData = json_decode($this->input->post('requestData'), true);
+            $this->load->model('API_model');
+            $query = $this->API_model->getList($requestData);
+            echo(json_encode($query));
+        }
+
+    }
+
+    public function getListItem()
+    {
+        if ($this->input->post('requestData') === null) {
+            http_response_code(400);
+            echo '<h1>400 Bad Request</h1>';
+        } else {
+            $requestData = json_decode($this->input->post('requestData'), true);
+            $this->load->model('API_model');
+            $query = $this->API_model->getListItem($requestData);
+            echo(json_encode($query));
+        }
+
     }
 
     public function updateList()
     {
-        if (!isset($_POST["updateData"])) {
+        if ($this->input->post('updateData') === null) {
             http_response_code(400);
             echo '<h1>400 Bad Request</h1>';
         } else {
-            $updateData = json_decode($_POST["updateData"], true);
+            $updateData = json_decode($this->input->post('updateData'), true);
             $this->load->model('API_model');
             $query = $this->API_model->updateList($updateData);
             echo $query;
