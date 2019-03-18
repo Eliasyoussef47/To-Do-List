9cb2614ec72f0d81e56e6e88fb4cfdd62032e262
Elias Youssef <d-s-a00@hotmail.com>
2019-03-18 21:48:03 +0100

    onclick toegevoegd aan newListBtn zodat het de modal open om een nieuwe lijst aan te maken

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,8 @@
 window.onload = function() {
     getAllLists().then(response => makeLists(response));
 };
+
+newListBtn.onclick = function () {
+    setUpModal(document.getElementById("editModal"), "Add a new list", "insertList", null);
+    $("#editModal").modal("show");
+};
