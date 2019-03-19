fafdc04a133d519077e8900d55162e7e2df10642
Elias Youssef <d-s-a00@hotmail.com>
2019-03-19 23:00:46 +0100

    had per ongeluk deleteListItem gezet bij de derde parameter, dat hoort niet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,6 @@ window.onload = function() {
 };
 
 newListBtn.onclick = function () {
-    setUpModal(document.getElementById("editModal"), "Add a new list", "deleteListItem", null);
+    setUpModal(document.getElementById("editModal"), "Add a new list", "insertList", null);
     $("#editModal").modal("show");
 };
