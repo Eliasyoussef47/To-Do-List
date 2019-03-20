edefac6ddf2086752f577db508d5434b440ca931
Elias Youssef <d-s-a00@hotmail.com>
2019-03-20 19:47:38 +0100

    listItemDuration wordt nou ook geupdate in listItem.dataset en dat help met sorteren updateListItemStatusVisual zodat het de dataset listItemStatus veranderd wanneer de checkbox geklikt wordt.

--- assets/js/visual server functions.js
+++ assets/js/visual server functions.js
@@ -20,6 +20,13 @@ function updateListItemVisual(updateData) {
     let listItem = list.querySelector("div.toDoListBody > div.listItem[data-list-item-id='" + updateData.listItemId + "']");
     let listItemTitle = listItem.querySelector("label.listItemTitle");
     listItemTitle.textContent = updateData.listItemName;
+    listItem.dataset.listItemDuration = updateData.listItemDuration;
+}
+
+function updateListItemStatusVisual(updateData) {
+    let list = document.querySelector("[data-list-id='" + updateData.listId + "']");
+    let listItem = list.querySelector("div.toDoListBody > div.listItem[data-list-item-id='" + updateData.listItemId + "']");
+    listItem.dataset.listItemStatus = (updateData.listItemStatus === true ? 1 : 0);
 }
 
 function deleteListVisual(deleteData) {
