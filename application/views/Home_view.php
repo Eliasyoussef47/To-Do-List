84de8a8b947896d2561e4349d636d83f37eb8432
Elias Youssef <d-s-a00@hotmail.com>
2019-03-26 11:26:41 +0100

    niet nodig. het wordt toch met js overgeschreven

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editModalTitle">Edit list</h5>
+                <h5 class="modal-title" id="editModalTitle"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
